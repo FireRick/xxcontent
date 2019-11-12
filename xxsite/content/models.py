99e78a2466dec197d6ac63ada5e47df98dc25c3f
fire <rixd@qq.com>
2019-11-12 12:00:24 +0800

    增加了文章页的阅读次数统计和显示（基于 redis），在模板中取消了标题下方的作者显示，精简内容

--- xxsite/content/models.py
+++ xxsite/content/models.py
@@ -3,6 +3,7 @@ import mistune
 from django.db import models
 from django.utils.safestring import mark_safe   # 防止html转义
 from django.template.loader import render_to_string
+import redis
 # from django.urls import reverse
 
 
@@ -66,6 +67,30 @@ class Article(TranMarkdown, models.Model):
 
     # def get_absolute_url(self):
     #     return reverse('article', args=[self.pk])
+
+    def save(self, *args, **kwargs):
+        """
+        在 redis 中设置 uv
+        """
+        super().save(*args, **kwargs)
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        if not r.exists('article:%s:uv' % self.id):
+            r.set('article:%s:uv' % self.id, 0)
+        return
+
+    @property
+    def get_uv(self):
+        """
+        获取文章的 uv，且每次在模板中调用 get_uv 时会对 uv 做 +1 操作。
+        在当前页面有缓存时不会重新渲染模板，所以不会造成同一用户频繁刷新造成 uv 暴增。
+        """
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        key = 'article:%s:uv' % self.id
+        if r.exists(key):
+            r.incr(key)
+            return str(r.get(key), encoding='utf-8')
+        else:
+            return "n/a"
 
     @classmethod
     def latest_articles(cls):
